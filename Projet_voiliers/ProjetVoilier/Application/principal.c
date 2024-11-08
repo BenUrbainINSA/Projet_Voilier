33d26fbcb0bf940d68e75d435b39622acd0d6181
LucasMnsl <lucas.mansilha41@gmail.com>
2024-11-08 10:56:23 +0100

    principal pour rotation plateau

--- Projet_voiliers/ProjetVoilier/Application/principal.c
+++ Projet_voiliers/ProjetVoilier/Application/principal.c
@@ -1,14 +1,26 @@
 #include "stm32f10x.h"
 #include "MyEncoder.h"
 #include "Driver_GPIO.h"
+#include "MyTimer.h"
 
 int main ( void )
 {
-	float Encoder_value;
-	MyEncoder_Base_Init(TIM2,4*360);
-	MyGPIO_Active_EXTI2_IT(0x0f,Reset_MyEncoder);   //reset l'encodeur quand Z vaut 1 !
+	//bit de sens
+	MyGPIO_Init (GPIOC, 3, Out_Ppull, Out10Mhz, 0);
+	MyGPIO_Reset (GPIOC, 3);
+	
+	//PWM plateau
+	MyGPIO_Init (GPIOA, 0, AltOut_Ppull, Out10Mhz, 0);
+	MyTimer_Base_Init(TIM2, 36, 100, My_timer_Up);
+	MyTimer_PWM(TIM2, 1, PWM_mode_1);
+	MyTimer_Base_Start(TIM2);
+	Set_PWM_Duty_Cycle(TIM2, 30, 1);
+	
+	
+	
+	
 	while (1)
 	{
-		Encoder_value=Get_MyEncoder_In_Deg(TIM2);
+		
 	}
 }
