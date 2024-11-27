a8625470f43ee182b2b8d5e84c7c66bef5f584ec
LucasMnsl <lucas.mansilha41@gmail.com>
2024-11-27 18:16:38 +0100

    spi début

--- Projet_voiliers/ProjetVoilier/Application/principal.c
+++ Projet_voiliers/ProjetVoilier/Application/principal.c
@@ -1,14 +1,26 @@
 #include "stm32f10x.h"
 #include "MyUSART.h"
 #include "ModulePlateau.h"
+#include "MySPI.h"
 
-int pwm =0;
+int data;
 
 int main ( void )
 {
-	ModulePlateau_Init();	
+	
+	char X0=0;
+	
+	MySPI_Init(SPI1);
+	MySPI_Set_NSS();
+	MySPI_Send(0x00);
+	
+	X0=MySPI_Read();
+
+	
+	MySPI_Clear_NSS();
+	
 	while (1)
 	{
-		ModulePlateau_Tourner();	
+		
 	}
 }
